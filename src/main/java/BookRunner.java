506c378a8feeebe7996fa4b9bfe1ca2c1f603b87
MIGUEL OLIVEIRA <miguel.oliveira.2001@gmail.com>
2019-10-23 20:29:28 +0100

    criação das classes

--- src/main/java/BookRunner.java
+++ src/main/java/BookRunner.java
@@ -1,7 +1,21 @@
+import models.Book;
+
 public class BookRunner {
 
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+
+        Book book1 = new Book();
+        book1.setTitle("THW art");
+
+
+        Book book2 = new Book();
+        book2.setTitle("ola art");
+
+        Book book3 = new Book();
+        book3.setTitle("ola");
+
+        System.out.println("O nome do livro é "+ book1.getTitle());
+        System.out.println("O nome do livro é "+ book2.getTitle());
+        System.out.println("O nome do livro é "+ book3.getTitle());
     }
 }
